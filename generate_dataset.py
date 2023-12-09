8a8673cb41a1025ace2ee047f45c37a31ad4bd65
sssirus <meizhishenzhi@163.com>
2023-12-09 18:17:57 +0800

    -v8

--- generate_dataset.py
+++ generate_dataset.py
@@ -1,6 +1,10 @@
 from examples.wordle.wordle import generate_random_words, generate_rand_choice_excluding
 
-
+import random
+import string
+def generate_random_string(length):
+    letters = string.ascii_letters.lower()  # 包含小写字母
+    return ''.join([random.choice(letters) for _ in range(length)])
 def generate_dataset(prompts,goal):
     #reward_fuction, prompts,samples_goal= generate_random_words(n_walks=length_of_train_data)#此函数是为了生成一系列prompts
     #_, test_prompts, test_samples_goal = generate_random_words(n_walks=len(prompts)/)  # 此函数是为了生成一系列prompt
@@ -30,13 +34,18 @@ def generate_dpo_dataset(prompts,goal):
     finally:
         file.close()
 
-    return  {
+    res=  {
         'train':{
             'prompt':prompts,
             'chosen':goal,
-            'prompt_chosen_rejected':[[prompts[i],goal[i],generate_rand_choice_excluding(goal[i], candidates)] for i in range(len(prompts))]
+            'prompt_chosen_rejected':[[prompts[i],goal[i],generate_rand_choice_excluding(goal[i], candidates)+generate_random_string(random.randint(0, 4))] for i in range(len(prompts))]
         }
     }
+    for i in range(len(prompts)):
+        res['train']['prompt'].append(prompts[i])
+        res['train']['chosen'].append(goal[i])
+        res['train']['prompt_chosen_rejected'].append([prompts[i],goal[i],generate_rand_choice_excluding(goal[i], candidates)[:random.randint(0, 4)]])
+    return res
     # file = open('data/wordle_words.txt', 'r')
     # candidates=[]
     # try:
